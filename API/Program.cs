7ebf317468398732dde8ed93275443244c334dd1
aaron dalao <aaron.dalao@gmail.com>
2023-06-03 02:00:10 +0800

    client section done

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,14 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+// added this 3/6/23
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +31,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
