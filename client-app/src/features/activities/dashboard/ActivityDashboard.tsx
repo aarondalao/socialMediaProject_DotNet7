ce752057e0661b33f170e96490b67c68500666ae
aaron dalao <aaron.dalao@gmail.com>
2023-06-05 11:52:49 +0800

    Activity Dashboard with its styling on the activity list and a hardcoded activity details done.

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app_common/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+
+interface Props {
+  activities: Activity[];
+}
+
+export default function ActivityDashboard({ activities }: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {activities[0] && <ActivityDetails activity={activities[0]} />}
+      </Grid.Column>
+    </Grid>
+  );
+}
