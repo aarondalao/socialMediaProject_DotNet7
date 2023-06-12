f9f5d93681750c2e656a36b13ad7998c93e30d23
aaron dalao <aaron.dalao@gmail.com>
2023-06-12 22:10:47 +0800

    refactor ActivityList.tsx and create a new Component ActivityListItem

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,64 +1,22 @@
-import React, { SyntheticEvent, useState } from "react";
-import { Button, Item, Label, Segment } from "semantic-ui-react";
+import React from 'react'
+import { Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app_common/stores/store";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
+import ActivityListItem from './ActivityListItem';
 
 export default observer( function ActivityList() {
   // from dataStore
   const {activityStore} = useStore();
 
   // destructured activityStore
-  const {deleteActivity, activitiesByDate, loading} = activityStore;
+  const { activitiesByDate} = activityStore;
+  
 
-  // target activity local state
-  const [target, setTarget] = useState("");
-
-
-  function handleDeleteActivity(
-    e: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  }
   return (
     <Segment>
       <Item.Group divided>
         {activitiesByDate.map((activity) => (
-          <Item key={activity.id}>
-            <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
-              <Item.Meta>{activity.date}</Item.Meta>
-              <Item.Description>
-                <div> {activity.description}</div>
-                <div>
-                  {" "}
-                  {activity.city}, {activity.venue}{" "}
-                </div>
-              </Item.Description>
-              <Item.Extra>
-                <Button
-                  as={Link}
-                  to={`/activities/${activity.id}`} 
-                  floated="right"
-                  content="View"
-                  color="blue"
-                />
-
-                {/* TODO: add alert button to double check deleting an event */}
-                <Button
-                  name={activity.id}
-                  loading={loading && target === activity.id}
-                  onClick={(e) => handleDeleteActivity(e, activity.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                />
-                <Label basic content={activity.category} />
-              </Item.Extra>
-            </Item.Content>
-          </Item>
+          <ActivityListItem key={activity.id} activity={activity} />
         ))}
       </Item.Group>
     </Segment>
