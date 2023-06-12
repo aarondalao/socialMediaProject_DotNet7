e70938012891c972719335dbd513d5c33e4eafd5
aaron dalao <aaron.dalao@gmail.com>
2023-06-13 07:33:13 +0800

    added grouping of activities by date and refactored the styling of activityListItem

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { Item, Segment } from "semantic-ui-react";
+import React, { Fragment } from "react";
+import { Header, Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app_common/stores/store";
 import { observer } from "mobx-react-lite";
-import ActivityListItem from './ActivityListItem';
+import ActivityListItem from "./ActivityListItem";
 
-export default observer( function ActivityList() {
+export default observer(function ActivityList() {
   // from dataStore
-  const {activityStore} = useStore();
+  const { activityStore } = useStore();
 
   // destructured activityStore
-  const { activitiesByDate} = activityStore;
-  
+  const { groupedActivities } = activityStore;
 
   return (
-    <Segment>
-      <Item.Group divided>
-        {activitiesByDate.map((activity) => (
-          <ActivityListItem key={activity.id} activity={activity} />
-        ))}
-      </Item.Group>
-    </Segment>
+    <>
+      {groupedActivities.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Header sub color="teal">
+            {group}
+          </Header>
+
+          {activities.map((activity) => (
+            <ActivityListItem key={activity.id} activity={activity} />
+          ))}
+        </Fragment>
+      ))}
+    </>
   );
-})
+});
