c03326af214af7a7a48ab7899fd893dcc85f3997
aaron dalao <aaron.dalao@gmail.com>
2023-06-14 08:19:00 +0800

    handling API response errors on Edit.cs done

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -35,7 +35,7 @@ namespace API.Controllers
         public async Task<IActionResult> EditActivity(Guid Id, Activity activity){
             activity.Id  = Id;
 
-            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+            return HandleResult(await Mediator.Send(new Edit.Command{Activity = activity}));
         }
 
         // api/activities/{id}
