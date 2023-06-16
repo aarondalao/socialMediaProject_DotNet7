482bef43a0fb0376eef7f0cfbeb52f714b44fdeb
aaron dalao <aaron.dalao@gmail.com>
2023-06-16 23:06:06 +0800

    created a reusable textInput, textArea, selectInput, and dateInput. installed a package react-datepicker

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,6 +5,7 @@ import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import reportWebVitals from "./reportWebVitals";
 import { StoreContext, store } from "./app/stores/store";
 import { RouterProvider } from "react-router-dom";
