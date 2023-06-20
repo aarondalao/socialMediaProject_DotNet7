de6341552d1b896f8fb5af0f3a3da46b10e2f1d7
Aaron Dalao <aaron.dalao@gmail.com>
2023-06-20 13:01:43 +0800

    created an AccountController.cs to the api layer to authenticate

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.DTOs
 {
     public class UserDto
     {
-        
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Image { get; set; }
+        public string Username { get; set; }
     }
 }
