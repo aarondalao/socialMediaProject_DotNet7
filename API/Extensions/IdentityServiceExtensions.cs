2f1dcd1c71ecba2594d132f6aa7116570be77e9c
Aaron Dalao <aaron.dalao@gmail.com>
2023-06-22 13:04:59 +0800

    added tokenService.cs | Server errors when generating token. error message : IDX10720: Unable to create KeyedHashAlgorithm for algorithm 'http://www.w3.org/2001/04/xmldsig-more#hmac-sha512', the key size must be greater than: '512' bits, key has '144' bits. (Parameter 'keyBytes')

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,3 +1,8 @@
+/*
+    services like transient and singleton. need to research about this
+*/
+
+using API.Services;
 using Domain;
 using Persistence;
 
@@ -13,6 +18,9 @@ namespace API.Extensions
             .AddEntityFrameworkStores<DataContext>();
 
             services.AddAuthentication();
+
+            // created 22/06/23 
+            services.AddScoped<TokenService>();
 
             return services;
         }
