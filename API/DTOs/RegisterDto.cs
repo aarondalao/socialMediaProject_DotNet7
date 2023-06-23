af1fa5377c89027778059c4eca4201de0af34573
Aaron Dalao <aaron.dalao@gmail.com>
2023-06-23 12:47:02 +0800

    validation for registering users done

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,12 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
+        public string DisplayName { get; set; }
+        
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }        
 
-        public string Password { get; set; }
-        public string DisplayName { get; set; }
+        [Required]
         public string Username { get; set; }
+
+         
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4-12}$", ErrorMessage = "Password must be 4-12 characters long, must has uppercase and lowercase")]
+        public string Password { get; set; }
+
 
     }
 }
