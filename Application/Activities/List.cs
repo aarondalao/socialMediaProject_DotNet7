290ee13179bdd4fc18afa5a75670a6bc993f2d0c
aarondalao <aaron.dalao@gmail.com>
2023-06-28 07:32:54 +0800

    data shaping and automapper profile configs
    Added new file
    - ActivityDto.cs -> a data transfer object responsible for shaping data that needs to be returned in a particular format.
    	similar with Activity.cs but with 2 added properties to attach related data of activity.cs
    - Profile.cs -> it is user profile to be used for the attendee property. this class has 4 properties and this will return when attendee property is needed.
    
    Modified files
    - MappingProfiles.cs -> mapping profile configurations 23d06233-878f-4df3-9040-8ed49b1a6620that will help us how to our data will be shaped into.
    - List.cs -> refactored Generic type from <Activity> to <ActivityDto> and used .ToProject() from AutoMapper.QueryableExtensions
    - Details.cs -> refactored Generic type from <Activity> to <ActivityDto> and used .ToProject() from Automapper.QueryableExtensions namespace

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -6,6 +6,8 @@
 
 */
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -18,25 +20,45 @@ namespace Application.Activities
     {
 
 
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
 
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
+                _mapper = mapper;
                 _context = context;
             }
-            
+
+
+
             // CancellationToken is used to handle cancelled request from a user either because the request is taking its time.
             // CancellationToken is provided by default when creating the request handler 
             // this is helpful if the data being fetched will take a long time for the user to receive or a fail-safe action 
             // when internal connection error is occured. 
-            public async Task<Result<List<Activity>>> Handle(Query request,CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+                // .Include gets all data fields that has autocreated using EF migrations in a 
+                // particular database, regardless whether it will be used or not.
+                // unnecessary bloat
+
+                // var activities = await _context.Activities
+                // .Include(a => a.Attendees)
+                // .ThenInclude(u => u.AppUser)
+                // .ToListAsync();
+
+                // ProjectTo method will get the necessary datafields that are specified in the mapping config
+                // and use that to create a query.
+                var activities = await _context.Activities.ProjectTo<ActivityDto>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+
+                // not needed if using .ProjectTo()
+                // var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
     }
