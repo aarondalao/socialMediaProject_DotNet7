157dd141ca107a278ab1c49f7bbe10de38bcf6d3
aarondalao <aaron.dalao@gmail.com>
2023-07-01 23:36:46 +0800

    minor ui changes
    added new axios API endpoint called attend
    added new method in activityStore.ts called updateAttendance()
    modified the button property in ActivityDetailedHeader.tsx to use updateAttendance onClick trigger

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -1,6 +1,16 @@
-export interface Profile{
-    username:string;
-    displayName:string;
-    image?:string;
+import { User } from "./User";
+
+export interface Profile {
+    username: string;
+    displayName: string;
+    image?: string;
     bio?: string;
+}
+
+export class Profile implements Profile {
+    constructor(user: User) {
+        this.username = user.username;
+        this.displayName = user.displayName;
+        this.image = user.image;
+    }
 }
