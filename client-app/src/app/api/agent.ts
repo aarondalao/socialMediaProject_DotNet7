cdf69cf78bd48367f83eb33b5a055e21721aefc4
aarondalao <37394846+aarondalao@users.noreply.github.com>
2023-07-16 21:17:13 +0800

    photo upload method completed
    files added:
    
    files modified:
    PhotoUploadWidget.tsx
    profileStore.tsx
    userStore.tsx
    ProfilePhotos.tsx

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -12,7 +12,7 @@ import { toast } from 'react-toastify';
 import { router } from '../router/Routes';
 import { store } from '../stores/store';
 import { User, UserFormValues } from '../models/User';
-import { Profile } from '../models/profile';
+import { Photo, Profile } from '../models/profile';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -109,7 +109,14 @@ const Account = {
 }
 
 const Profiles = {
-    get: (username: string ) => requests.get<Profile>(`/profiles/${username}`)
+    get: (username: string ) => requests.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-Type': 'multipart/form-data'}
+        }) 
+    }
 }
 
 const agent = {
