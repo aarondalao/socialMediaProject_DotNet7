e161a4a1e4ac252e82cf7053414fb56ff030b95e
aarondalao <37394846+aarondalao@users.noreply.github.com>
2023-07-18 07:30:40 +0800

    edit.cs and ProfilesController.cs done

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -3,11 +3,21 @@ using Application.Profiles;
 
 namespace API.Controllers
 {
-    public class ProfilesController : BaseApiController{
-    
-    [HttpGet("{username}")]
-    public async Task<IActionResult> GetProfile(string username){
-            return HandleResult(await Mediator.Send(new Details.Query{Username = username}));
+    public class ProfilesController : BaseApiController
+    {
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
         }
+        [HttpPut]
+        public async Task<IActionResult> EditProfileDisplayName(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
+
     }
+
+
 }
--- Application/Profiles/Edit.cs
+++ Application/Profiles/Edit.cs
@@ -1,6 +1,9 @@
 using Application.Core;
+using Application.Interfaces;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace Application.Profiles
 {
@@ -8,15 +11,50 @@ namespace Application.Profiles
     {
         public class Command : IRequest<Result<Unit>>
         {
-            public Profile profile { get; set; }
+            public string DisplayName { get; set; }
+            public string Bio { get; set; }
         }
-
         // validation class 
         public class CommandValidator : AbstractValidator<Command>
         {
             public CommandValidator()
             {
-                RuleFor(x => x.profile).SetValidator(new ProfileValidator());
+                // RuleFor(x => x.DisplayName).SetValidator(new ProfileValidator());
+                RuleFor(x => x.DisplayName).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly IUserAccessor _userAccessor;
+            private readonly DataContext _context;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+            
+                if (user == null) return null;
+
+                
+                user.Bio = request.Bio ?? user.Bio;
+                user.DisplayName = request.DisplayName ?? user.DisplayName;
+
+                // current behavior is this wil throw 400 bad request if no changes were made on bio or display name.
+                // to change this into always get 200 status code regardless of whether there were changes made,
+
+                _context.Entry(user).State = EntityState.Modified;
+
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to update ");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
