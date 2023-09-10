ceff820c3eaf04623f04680991c46580ae3e3d99
aarondalao <37394846+aarondalao@users.noreply.github.com>
2023-09-10 14:22:00 +0800

    Finishing touches
     - fixed that annoying feature whenever we load an activity or profile, the scroll bar is either at the middle or at the bottom, making the ui not rendered properly at first load. This fix will make the scroll bar back to top at first loads.
     - fixed the routes to introduce private routing where we'll only expose certain routes for unauthenticated users to restrict actions.

--- client-app/src/app/layout/navBar.tsx
+++ client-app/src/app/layout/navBar.tsx
@@ -5,51 +5,65 @@ import { observer } from "mobx-react-lite";
 
 export default observer(function NavBar() {
   const {
-    userStore: { user, logout },
+    userStore: { user, logout, isLoggedIn },
   } = useStore();
 
   return (
     <Menu inverted fixed="top">
-      <Container>
-        <Menu.Item as={NavLink} to="/" header>
-          <img
-            src="/assets/logo.png"
-            alt="logo"
-            style={{ marginRight: "30px" }}
-          />
-          How-to Club
-        </Menu.Item>
-        <Menu.Item as={NavLink} to="/activities" name="Activities" />
-        <Menu.Item as={NavLink} to="/errors" name="Errors" />
-        <Menu.Item>
-          <Button
-            positive
-            content="Create Activity"
-            // onClick={() => activityStore.openForm()}
-            as={NavLink}
-            to="/createActivity"
-          />
-        </Menu.Item>
+      {isLoggedIn ? (
+        <Container>
+          <Menu.Item as={NavLink} to="/" header>
+            <img
+              src="/assets/logo.png"
+              alt="logo"
+              style={{ marginRight: "30px" }}
+            />
+            How-to Club
+          </Menu.Item>
 
-        <Menu.Item position="right">
-          <Image
-            src={user?.image || "/assets/user.png"}
-            avatar
-            spaced="right"
-          />
-          <Dropdown pointing="top left" text={user?.displayName}>
-            <Dropdown.Menu>
-              <Dropdown.Item
-                as={Link}
-                to={`/profiles/${user?.username}`}
-                text="My Profile"
-                icon="user"
-              />
-              <Dropdown.Item onClick={logout} text="Logout" incon="power" />
-            </Dropdown.Menu>
-          </Dropdown>
-        </Menu.Item>
-      </Container>
+          <Menu.Item as={NavLink} to="/activities" name="Activities" />
+          <Menu.Item as={NavLink} to="/errors" name="Errors" />
+          <Menu.Item>
+            <Button
+              positive
+              content="Create Activity"
+              // onClick={() => activityStore.openForm()}
+              as={NavLink}
+              to="/createActivity"
+            />
+          </Menu.Item>
+
+          <Menu.Item position="right">
+            <Image
+              src={user?.image || "/assets/user.png"}
+              avatar
+              spaced="right"
+            />
+            <Dropdown pointing="top left" text={user?.displayName}>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  as={Link}
+                  to={`/profiles/${user?.username}`}
+                  text="My Profile"
+                  icon="user"
+                />
+                <Dropdown.Item onClick={logout} text="Logout" incon="power" />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+        </Container>
+      ): (
+        <Container>
+          <Menu.Item as={NavLink} to="/" header>
+            <img
+              src="/assets/logo.png"
+              alt="logo"
+              style={{ marginRight: "30px" }}
+            />
+            How-to Club
+          </Menu.Item>
+        </Container>
+      )}
     </Menu>
   );
 });
